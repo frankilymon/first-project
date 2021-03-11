b906ef3bf3fdd3e43b021f465673e21900be53df
frankilymon <frank_ukaegbu@yahoo.com>
2021-03-10 22:19:10 -0500

    Added username property, created button on reset

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,16 +2,30 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
-  styleUrls: ['./servers.component.css']
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  styleUrls: ['./servers.component.css'],
+  templateUrl: './servers.component.html'
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
 
   ngOnInit(): void {
   }
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+  onUpdateServerName( event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
 
+  }
 }
